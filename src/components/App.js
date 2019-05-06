af211300d8858d05207ac9c65fe0da338699753d
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-06 23:37:03 +0200

    Cleanup...

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Container from 'components/Container';
 
 export default function App() {
   return (
     <>
-
+      <h1> Hello </h1>
+      <Container />
     </>
   );
 }
