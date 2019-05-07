81156668f69ee57dcd23e5e019b7ae7419ba937a
Marcus Francis <marcusmagnusson@outlook.com>
2019-05-07 04:21:19 +0200

    + List component

---
+++ src/components/List.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+export default function List(props) {
+  const ProductItems = props.products.map(product => (
+    <div class="column">
+      <div class="ui centered card">
+        <img
+          class="ui centered small image"
+          src={`products/${product.sku}.jpg`}
+          alt={product.title}
+        />
+        <div class="content">
+          <a class="header"> {product.title} </a>
+          <div class="description">{product.description}</div>
+        </div>
+        <div class="extra content">
+          <a>{product.price + " kr "}</a>
+          <button
+            class="ui basic right floated button"
+            onClick={() => props.addProduct(product)} >
+            <i class="shopping cart icon" />
+            Lägg till
+          </button>
+        </div>
+      </div>
+    </div>
+  ));
+
+  return (
+    <>
+      <div class="ui three column doubling stackable grid container">
+        {ProductItems}
+      </div>
+    </>
+  );
+}
